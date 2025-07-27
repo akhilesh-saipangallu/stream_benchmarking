a0e08ca69d0fb084d8901ed477b8517e4eef485f
Akhilesh Saipangallu <akhilesh.saipangallu@redis.com>
2025-07-27 12:15:57 +0530

    * updated ws client script to run multiple clients at the same time * updated prometheus to collect ws client metrics

--- ws_client_sample.go
+++ ws_client_sample.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/akhilesh-saipangallu/stream_benchmarking/config"
@@ -12,9 +17,114 @@ import (
 	"github.com/akhilesh-saipangallu/stream_benchmarking/utils"
 	"github.com/akhilesh-saipangallu/stream_benchmarking/ws"
 	"github.com/gorilla/websocket"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+type WsClientCmdArgs struct {
+	suffix     *string
+	separator  *string
+	numTickers *int
+}
+
+func generateTickers(cmdArgs WsClientCmdArgs) (tickers []string) {
+	for i := 0; i < *cmdArgs.numTickers; i++ {
+		ticker := fmt.Sprintf("%s%s%d", *cmdArgs.suffix, *cmdArgs.separator, i)
+		tickers = append(tickers, ticker)
+	}
+	return
+}
+
+type WsClient struct {
+	conn               *websocket.Conn
+	rLogger            utils.LogOnceEveryN
+	tickersToSubscribe []string
+}
+
+func NewWsClient(tickersToSubscribe []string) *WsClient {
+	return &WsClient{
+		rLogger:            *utils.NewLogOnceEveryN(100),
+		tickersToSubscribe: tickersToSubscribe,
+	}
+}
+
+func (c *WsClient) Start(url string) (err error) {
+	err = c.Connect(url)
+	if err != nil {
+		return err
+	}
+
+	err = c.Subscribe()
+	if err != nil {
+		return err
+	}
+	go c.Reader()
+
+	return
+}
+
+func (c *WsClient) Connect(url string) (err error) {
+	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		return fmt.Errorf("WsClient.Start: dial error: %w", err)
+	}
+	c.conn = conn
+	return
+}
+
+func (c *WsClient) Subscribe() (err error) {
+	subMsg := ws.SubscriptionMessage{
+		Action:  "subscribe",
+		Tickers: c.tickersToSubscribe,
+	}
+	err = c.conn.WriteJSON(subMsg)
+	if err != nil {
+		err = fmt.Errorf("WsClient.Subscribe: write error: %w", err)
+	}
+	return
+}
+
+func (c *WsClient) Reader() {
+	for {
+		_, msg, err := c.conn.ReadMessage()
+		if err != nil {
+			log.Println("read error:", err)
+			return
+		}
+
+		now := time.Now().UnixMicro()
+		pm, err := datatype.ParsePriceMessageBytes(msg)
+		if err != nil {
+			log.Printf("WsClient.Reader: error: %s\n", err)
+			continue
+		}
+		latency := float64(now - pm.Timestamp) // latency in µs
+		metrics.EndToEndLatency.Observe(latency)
+
+		// log.Printf("received: %s", msg)
+		c.rLogger.Log("received: %s", msg)
+	}
+}
+
+func (c WsClient) Stop() {
+	c.conn.Close()
+}
+
 func main() {
+	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+
+	numWsClientsPtr := flag.Int("client_count", 100, "Number of WS clients to create")
+	numTickersPtr := flag.Int("ticker_count", 300, "Number of tickers to subscribe")
+	suffixPtr := flag.String("suffix", "nifty", "Ticker suffix")
+	separatorPtr := flag.String("sep", "_", "Ticker separator")
+	flag.Parse()
+
+	cmdArgs := WsClientCmdArgs{
+		suffix:     suffixPtr,
+		separator:  separatorPtr,
+		numTickers: numTickersPtr,
+	}
+
+	metrics.InitNatsMetrics()
 	config.Init()
 	cfg, err := config.Get()
 	if err != nil {
@@ -22,168 +132,34 @@ func main() {
 		return
 	}
 
+	http.Handle("/metrics", promhttp.Handler())
+	go http.ListenAndServe(":2212", nil)
+
 	url := cfg.WsServer.WSEndpoint
+	tickers := generateTickers(cmdArgs)
 
-	rLogger := utils.NewLogOnceEveryN(100)
-
-	log.Printf("connecting to %s", url)
-	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
-	if err != nil {
-		log.Fatal("dial error:", err)
-	}
-	defer conn.Close()
-
-	// Example: Send a subscription message
-	subMsg := ws.SubscriptionMessage{
-		Action: "subscribe",
-		Tickers: []string{
-			"bn_0",
-			"bn_1",
-			"bn_2",
-			"bn_3",
-			"bn_4",
-			"bn_5",
-			"bn_6",
-			"bn_7",
-			"bn_8",
-			"bn_9",
-			"bn_10",
-			"bn_11",
-			"bn_12",
-			"bn_13",
-			"bn_14",
-			"bn_15",
-			"bn_16",
-			"bn_17",
-			"bn_18",
-			"bn_19",
-			"bn_20",
-			"bn_21",
-			"bn_22",
-			"bn_23",
-			"bn_24",
-			"bn_25",
-			"bn_26",
-			"bn_27",
-			"bn_28",
-			"bn_29",
-			"bn_30",
-			"bn_31",
-			"bn_32",
-			"bn_33",
-			"bn_34",
-			"bn_35",
-			"bn_36",
-			"bn_37",
-			"bn_38",
-			"bn_39",
-			"bn_40",
-			"bn_41",
-			"bn_42",
-			"bn_43",
-			"bn_44",
-			"bn_45",
-			"bn_46",
-			"bn_47",
-			"bn_48",
-			"bn_49",
-			"bn_50",
-			"bn_51",
-			"bn_52",
-			"bn_53",
-			"bn_54",
-			"bn_55",
-			"bn_56",
-			"bn_57",
-			"bn_58",
-			"bn_59",
-			"bn_60",
-			"bn_61",
-			"bn_62",
-			"bn_63",
-			"bn_64",
-			"bn_65",
-			"bn_66",
-			"bn_67",
-			"bn_68",
-			"bn_69",
-			"bn_70",
-			"bn_71",
-			"bn_72",
-			"bn_73",
-			"bn_74",
-			"bn_75",
-			"bn_76",
-			"bn_77",
-			"bn_78",
-			"bn_79",
-			"bn_80",
-			"bn_81",
-			"bn_82",
-			"bn_83",
-			"bn_84",
-			"bn_85",
-			"bn_86",
-			"bn_87",
-			"bn_88",
-			"bn_89",
-			"bn_90",
-			"bn_91",
-			"bn_92",
-			"bn_93",
-			"bn_94",
-			"bn_95",
-			"bn_96",
-			"bn_97",
-			"bn_98",
-			"bn_99",
-		},
-	}
-	err = conn.WriteJSON(subMsg)
-	if err != nil {
-		log.Println("write error:", err)
-		return
-	}
-
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
-
-	go func() {
-		for {
-			now := time.Now().UnixMicro()
-
-			_, msg, err := conn.ReadMessage()
-			if err != nil {
-				log.Println("read error:", err)
-				return
-			}
-
-			pm, err := datatype.ParsePriceMessageBytes(msg)
-			if err != nil {
-				return
-			}
-			latency := float64(now - pm.Timestamp) // latency in µs
-			metrics.EndToEndLatency.Observe(latency)
-
-			// log.Printf("received: %s", msg)
-			rLogger.Log("received: %s", msg)
+	var (
+		totalConnectedClients int
+		wsClients             []*WsClient
+	)
+	for i := 0; i < *numWsClientsPtr; i++ {
+		wsClient := NewWsClient(tickers)
+		wsErr := wsClient.Start(url)
+		if wsErr != nil {
+			log.Printf("==ERROR==: failed to initiate ws client id:%d\n", i)
+			continue
 		}
-	}()
-
-	// Keep the client running until interrupted
-	for {
-		select {
-		case <-interrupt:
-			log.Println("interrupt received; closing connection...")
-
-			// Optional: Send close message
-			err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-			if err != nil {
-				log.Println("close error:", err)
-				return
-			}
-			time.Sleep(time.Second)
-			return
-		}
+		totalConnectedClients += 1
+		wsClients = append(wsClients, wsClient)
 	}
+	log.Println("==INFO==: totalConnectedClients:", totalConnectedClients)
+
+	// wait for interrupt
+	<-ctx.Done()
+	log.Println("interrupt received; closing connection...")
+
+	for _, wsClient := range wsClients {
+		wsClient.Stop()
+	}
+	log.Println("stopped all WS clients; exiting")
 }
